45afda9b7941c2378fe70ebbc37d5f1a374e0e16
nevermind1985 <78436967+nevermind1985@users.noreply.github.com>
2022-04-19 10:46:54 +0200

    plugin definitivo
    Plugin definitivo

--- js/time_calculator_news.js
+++ js/time_calculator_news.js
@@ -4,16 +4,19 @@
 
 -test newsAnimation
 */
-var newsCookieValue = "BrowserExtension";
+var newsCookieValue = "BrowserExtensionNew";
 
 function checkForNews() {
 	if(newsCookieValue != "") {
 		var news = getCookie("news");
 		if((news != "" && news != null && news != newsCookieValue) || (news == null)) {
-			setCookie("news", newsCookieValue);
 			document.getElementById('newsContainer').classList.add(newsCookieValue);
 			document.getElementById('newsContainer').style.display = 'block';
 			document.getElementById('newsContainer').style.animationName = newsCookieValue;
 		}
 	}
+}
+
+function disableNews() {
+	setCookie("news", newsCookieValue);
 }
